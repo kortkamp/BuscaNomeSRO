2cba957c69bd085e73eb03039f2bb17168c6b565
kortkamp <marcelusmedius@gmail.com>
2021-06-28 15:48:08 -0300

    feat( crawler ): search name

--- crawler.js
+++ crawler.js
@@ -1,8 +1,75 @@
+const axios = require('axios');
+
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// not implemented yet
+const url = 'http://10.8.88.217/rastreamento/sro';
+
+
+
+
+
+// faz o body baseando-se na primeira resposta de pesquisa
+function makeBody(data,codObjeto){
+
+    const position = data.search("','PO','")
+
+    codOperacao =  'PO';
+    codRegistro =  data.substring(position + 8,position + 18);
+    tipoRegistro = data.substring(position + 21 , position + 22);
+    dataCriacao = data.substring(position + 25, position + 44);
+
+
+    const body = {
+        opcao:'DETALHESINTRA',
+        idioma: '001',
+        ambiente: 'INTRA',
+        codItem: codObjeto,
+        codOperacao: 'PO',
+        codRegistro:codRegistro,
+        tipoRegistro:tipoRegistro,
+        dataCriacao:dataCriacao,
+        portal:'intra'
+    }
+
+    return body;
+}
+
+function searchName(data){
+    const initPosition = data.indexOf('Destinat',0) + 25;
+    const finalPosition = data.indexOf('</TD>', initPosition) - 5;
+
+    const nome = data.substring(initPosition,finalPosition);
+
+    return nome;
+}
+
+
+
+async function getList(codObjeto){
+    axios.post(url,{
+        opcao: 'PESQUISA',
+        portal: 'intra',
+        objetos: codObjeto
+      }).then(result => {
+
+          const body = makeBody(result.data,codObjeto);
+
+          console.log(body)
+
+          axios.post(url,body).then(result =>{
+
+              const name = searchName(result)
+              console.log(name)
+              return name;
+          })
+          
+
+          
+
+      });
+}
 
 async function getObjectCustomerName(code){
     console.log("++++")
@@ -11,4 +78,4 @@ async function getObjectCustomerName(code){
 }
 
 
-module.exports = {getObjectCustomerName}
+module.exports = {getObjectCustomerName, getList}
